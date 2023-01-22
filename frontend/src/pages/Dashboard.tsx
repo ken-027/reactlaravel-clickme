3f91e0d594b1add4f328d3d4d2fbc95e30d452e8
ken <82031953+ken-027@users.noreply.github.com>
2023-01-22 09:26:46 +0800

    FRONTEND: interface

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import '../styles/sass/components/_card.scss'
 
 function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className='container'>
+      <div className='caption'>
+        <h1>React x Laravel</h1>
+        <p>Click Me!</p>
+      </div>
+      <section className='card'>
+        <button className='title'>Click Me!</button>
+        <p className='detail'>0</p>
+        <p className='footer'>12:00:01 Saturday 1/22/2023</p>
+      </section>
     </div>
   )
 }
